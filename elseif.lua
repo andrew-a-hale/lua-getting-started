a31364c4752663a6f83e3d3d8fe7a94ba471bd8c
Andrew Hale <andrew.hale003@gmail.com>
2023-05-14 11:01:55 +1000

    oops

---
+++ elseif.lua
@@ -0,0 +1,12 @@
+dice = 5
+
+-- elseif
+if dice > 1 then
+    print("dice > 1")
+elseif dice > 3 then
+    print("dice > 3")
+elseif dice == 5 then
+    print("dice == 5")
+else 
+    print("dice is 4 or 6")
+end
---
+++ if.lua
@@ -0,0 +1,14 @@
+dice = 5
+
+-- ifs
+if dice > 1 then
+    print("dice > 1")
+end
+
+if dice > 3 then
+    print("dice > 3")
+end
+
+if dice == 5 then
+    print("dice == 5")
+end
