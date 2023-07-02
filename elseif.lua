0755bc925712982ab60fd02d9282202d2fc2222e
Andrew Hale <andrew.hale003@gmail.com>
2023-07-02 15:47:49 +1000

    update

--- elseif.lua
+++ elseif.lua
@@ -1,6 +1,8 @@
 dice = 5
 
 -- elseif
+-- in this case only the the first true elseif will be executed
+-- this is because dice > 3 and dice > 5 are true if dice > 1 which is always executed first
 if dice > 1 then
     print("dice > 1")
 elseif dice > 3 then
@@ -8,5 +10,5 @@ elseif dice > 3 then
 elseif dice == 5 then
     print("dice == 5")
 else 
-    print("dice is 4 or 6")
+    print("dice <= 1")
 end
--- if.lua
+++ if.lua
@@ -1,6 +1,7 @@
 dice = 5
 
 -- ifs
+-- in this case all 3 ifs will be executed
 if dice > 1 then
     print("dice > 1")
 end
